d055e70d851f2798c69bc8d4ee7b38a46127c664
Scott White <whitews@gmail.com>
2013-01-04 09:45:07 -0500

    add_subject.html: - new template for adding new subjects
    view_project.html:
    - Make title consistent showing badge for subject count
    - display the site for each subject
    
    forms.py:
    - new form for the Subject model
    
    urls.py:
    - new route for add_subject view
    
    views.py:
    - new view for adding a new subject

--- repository/forms.py
+++ repository/forms.py
@@ -30,3 +30,21 @@ class PanelForm(ModelForm):
             if not sites:
                 raise ValidationError('Error creating panel. There are no sites for this project. A panel must belong to a project site.')
             self.fields['site'] = ModelChoiceField(sites)
+
+class SubjectForm(ModelForm):
+    class Meta:
+        model = Subject
+
+    def __init__(self, *args, **kwargs):
+        # pop our 'project_id' key since parent's init is not expecting it
+        project_id = kwargs.pop('project_id', None)
+
+        # now it's safe to call the parent init
+        super(SubjectForm, self).__init__(*args, **kwargs)
+
+        # finally, the reason we're here...make sure only the project's sites are the available choices
+        if project_id:
+            sites = Site.objects.filter(project__id=project_id)
+            if not sites:
+                raise ValidationError('Error creating panel. There are no sites for this project. A subject must belong to a project site.')
+            self.fields['site'] = ModelChoiceField(sites)
