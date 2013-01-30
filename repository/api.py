17269cc26903461a0c4d10a9a201ed0df6ceeeb7
Scott White <whitews@gmail.com>
2013-01-30 13:29:08 -0500

    api.py: - added basic authentication to all ModelResource classes

--- repository/api.py
+++ repository/api.py
@@ -1,4 +1,5 @@
 from tastypie import fields
+from tastypie.authentication import BasicAuthentication
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from repository.models import *
 
@@ -10,6 +11,7 @@ class ProjectResource(ModelResource):
             'id': ALL_WITH_RELATIONS,
             'project_name': ALL_WITH_RELATIONS,
         }
+        authentication = BasicAuthentication()
 
 class SiteResource(ModelResource):
     project = fields.ToOneField(ProjectResource, 'project')
@@ -21,6 +23,7 @@ class SiteResource(ModelResource):
             'id': ALL_WITH_RELATIONS,
             'project': ALL_WITH_RELATIONS,
         }
+        authentication = BasicAuthentication()
 
 class PanelResource(ModelResource):
     project = fields.ToOneField(ProjectResource, 'project')
@@ -31,6 +34,7 @@ class PanelResource(ModelResource):
         filtering = {
             'project': ALL_WITH_RELATIONS,
         }
+        authentication = BasicAuthentication()
 
 class SubjectResource(ModelResource):
     site = fields.ToOneField(SiteResource, 'site')
@@ -42,6 +46,7 @@ class SubjectResource(ModelResource):
             'subject_id': ALL_WITH_RELATIONS,
             'site': ALL_WITH_RELATIONS,
         }
+        authentication = BasicAuthentication()
 
 class SampleResource(ModelResource):
     subject = fields.ToOneField(SubjectResource, 'subject')
@@ -58,6 +63,7 @@ class SampleResource(ModelResource):
             'subject': ALL_WITH_RELATIONS,
             'parameters': ALL_WITH_RELATIONS,
         }
+        authentication = BasicAuthentication()
 
 #    def dehydrate(self, bundle):
 #        sample_parameter_set = SampleParameterMap.objects.filter(sample=bundle.obj.pk)
@@ -83,6 +89,7 @@ class ParameterResource(ModelResource):
             'samples': ALL_WITH_RELATIONS,
             'fcs_text': ALL,
         }
+        authentication = BasicAuthentication()
 
 class SampleParameterMapResource(ModelResource):
     sample = fields.ToOneField(SampleResource, 'sample')
@@ -96,11 +103,13 @@ class SampleParameterMapResource(ModelResource):
             'parameter': ALL_WITH_RELATIONS,
             'fcs_text': ALL,
         }
+        authentication = BasicAuthentication()
 
 class AntibodyResource(ModelResource):
     class Meta:
         queryset = Antibody.objects.all()
         resource_name = 'antibody'
+        authentication = BasicAuthentication()
 
 class ParameterAntibodyMapResource(ModelResource):
     parameter = fields.ToOneField(ParameterResource, 'parameter')
@@ -109,3 +118,4 @@ class ParameterAntibodyMapResource(ModelResource):
     class Meta:
         queryset = ParameterAntibodyMap.objects.all()
         resource_name = 'parameter_antibody'
+        authentication = BasicAuthentication()
