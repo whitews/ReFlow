7e175185fc6955fffee396c52ba72fcbd9a97498
Scott White <whitews@gmail.com>
2013-03-13 16:50:18 -0400

    think I need to do a bit more to ensure proper admin behavior, was getting some empty HTTP responses when trying to delete a sample in the admin interface, though it was only happening under Apache/mod_wsgi and not the dev server, hope this does the trick

--- repository/admin.py
+++ repository/admin.py
@@ -13,9 +13,33 @@ admin.site.register(ParameterValueType)
 admin.site.register(ProjectVisitType)
 admin.site.register(Site)
 admin.site.register(Subject)
-admin.site.register(Sample)
 
 admin.site.register(ParameterAntibodyMap)
 admin.site.register(ParameterFluorochromeMap)
 admin.site.register(PanelParameterMap)
-admin.site.register(SampleParameterMap)
+
+
+class SampleAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        qs = Sample.admin  #Use the admin manager regardless of what the default one is
+
+        # we need this from the superclass method
+        ordering = self.ordering or ()
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+admin.site.register(Sample, SampleAdmin)
+
+
+class SampleParameterMapAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        qs = SampleParameterMap.admin  #Use the admin manager regardless of what the default one is
+
+        # we need this from the superclass method
+        ordering = self.ordering or ()
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+admin.site.register(SampleParameterMap, SampleParameterMapAdmin)
