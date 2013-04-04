5f69af78c34d5ef697fff4100b32a247f95b9e74
Scott White <whitews@gmail.com>
2013-04-04 14:14:20 -0400

    view_project_samples.html, view_subject.html: - fix permission checking and make sample list table the same between templates
    views.py:
    - view_subject:
      - don't use @permission_required decorator, use custom @require_project_or_site_view_permission
      - filter subject samples by sites the request.user has access to

--- repository/views.py
+++ repository/views.py
@@ -135,6 +135,7 @@ def view_samples(request, project_id):
             'id',
             'subject__subject_id',
             'site__site_name',
+            'site__id',
             'visit__visit_type_name',
             'original_filename'
         )
@@ -151,7 +152,6 @@ def view_samples(request, project_id):
         for site in user_view_sites:
             sites = get_objects_for_user(request.user, 'add_site_data', klass=Site) \
                 .filter(project_id=project_id).order_by('site_name')
-
 
     spm_maps = SampleParameterMap.objects.filter(sample_id__in=[i['id'] for i in samples]).values(
         'id',
@@ -588,16 +588,34 @@ def create_panel_from_sample(request, sample_id):
 
 
 @login_required
-@permission_required('view_project_data', (Project, 'id', 'project_id'), return_403=True)
+@require_project_or_site_view_permission
 def view_subject(request, project_id, subject_id):
     subject = get_object_or_404(Subject, pk=subject_id, project_id=project_id)
     project = subject.project
 
-    samples = Sample.objects.filter(subject=subject).values(
-        'id',
-        'site__site_name',
-        'visit__visit_type_name',
-        'original_filename')
+    # get user's sites based on their site_view_permission, unless they have full project view permission
+    if project.has_view_permission(request.user):
+        samples = Sample.objects.filter(subject=subject).values(
+            'id',
+            'subject__subject_id',
+            'site__site_name',
+            'site__id',
+            'visit__visit_type_name',
+            'original_filename'
+        )
+    else:
+        user_view_sites = Site.objects.get_sites_user_can_view(request.user, project=project)
+        samples = Sample.objects.filter(subject=subject, site__in=user_view_sites).values(
+            'id',
+            'subject__subject_id',
+            'site__site_name',
+            'site__id',
+            'visit__visit_type_name',
+            'original_filename'
+        )
+        for site in user_view_sites:
+            sites = get_objects_for_user(request.user, 'add_site_data', klass=Site) \
+                .filter(project_id=project_id).order_by('site_name')
 
     spm_maps = SampleParameterMap.objects.filter(sample_id__in=[i['id'] for i in samples]).values(
         'id',
@@ -615,6 +633,8 @@ def view_subject(request, project_id, subject_id):
 
     can_add_project_data = project.has_add_permission(request.user)
     can_modify_project_data = project.has_modify_permission(request.user)
+    user_add_sites = Site.objects.get_sites_user_can_add(request.user, project).values_list('id', flat=True)
+    user_modify_sites = Site.objects.get_sites_user_can_modify(request.user, project).values_list('id', flat=True)
 
     return render_to_response(
         'view_subject.html',
@@ -624,6 +644,8 @@ def view_subject(request, project_id, subject_id):
             'samples': samples,
             'can_add_project_data': can_add_project_data,
             'can_modify_project_data': can_modify_project_data,
+            'user_add_sites': user_add_sites,
+            'user_modify_sites': user_modify_sites
         },
         context_instance=RequestContext(request)
     )
