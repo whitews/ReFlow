e1669be48816fad90e8211b61db1b2b33083413f
Scott White <whitews@gmail.com>
2013-06-19 16:28:47 -0400

    few more project level unit tests

--- repository/tests/unit_tests.py
+++ repository/tests/unit_tests.py
@@ -8,6 +8,8 @@ from django.conf.global_settings import FILE_UPLOAD_TEMP_DIR
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+from guardian.shortcuts import assign_perm
+
 from repository.models import *
 from repository.forms import *
 from repository.tests import constants
@@ -15,6 +17,7 @@ from repository.tests import constants
 
 def testSetup():
     User.objects.create_superuser('supertester', password='supertester', email=None)
+    User.objects.create_user('tester', password='tester', email=None)
 
 
 @override_settings(MEDIA_ROOT=FILE_UPLOAD_TEMP_DIR)
@@ -22,9 +25,39 @@ class ModelsUnitTestCase(TestCase):
 
     fixtures = ['repository_models_tests.json']
 
+    def setUp(self):
+        testSetup()
+        self.test_user = User.objects.get(username='tester')
+        self.test_project = Project.objects.get(project_name='Project T')
+
+    def test_user_has_no_projects(self):
+        user_projects = Project.objects.get_projects_user_can_view(self.test_user)
+        self.assertEqual(len(user_projects), 0)
+
+    def test_user_has_projects(self):
+        assign_perm('view_project_data', self.test_user, project)
+        user_projects = Project.objects.get_projects_user_can_view(self.test_user)
+        self.assertEqual(len(user_projects), 1)
+        self.assertEqual(project.id, user_projects[0].id)
+
+    def test_user_has_projects_based_on_site_access(self):
+        projectX = Project.objects.get(project_name='Project X')
+        projectX_site = Site.objects.get(site_name='Atlantis', project=projectX)
+        assign_perm('view_site_data', self.test_user, projectX_site)
+        user_projects = Project.objects.get_projects_user_can_view(self.test_user)
+        self.assertEqual(len(user_projects), 1)
+
+
+
+
     def test_antibody_model(self):
         antibody = Antibody(antibody_short_name='IL-6')
-        self.assertRaises(ValidationError, antibody.save)
+        with self.assertRaises(ValidationError) as context:
+            antibody.full_clean()
+
+        self.assertEqual(
+            context.exception.message_dict,
+            {'antibody_name': [u'This field cannot be blank.']})
 
 
 @override_settings(MEDIA_ROOT=FILE_UPLOAD_TEMP_DIR)
