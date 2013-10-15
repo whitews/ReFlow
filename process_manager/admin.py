d2984584cc1584b6a46c8d1b0bbeebb2ac902048
Scott White <whitews@gmail.com>
2013-10-15 14:23:04 -0400

    remove some process manager models from admin

--- process_manager/admin.py
+++ process_manager/admin.py
@@ -3,10 +3,8 @@ from django.contrib import admin
 
 admin.site.register(Process)
 admin.site.register(ProcessInput)
-admin.site.register(ProcessOutput)
 
 admin.site.register(Worker)
-admin.site.register(WorkerProcessMap)
 
 admin.site.register(ProcessRequest)
 admin.site.register(ProcessRequestInputValue)
