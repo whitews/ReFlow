11c4d37af853d7ce3aabc330eef0a3b9fd465397
Scott White <whitews@gmail.com>
2014-06-20 11:27:06 -0400

    added current sample and panel template to ModelService

--- static/ng-services/modelService.js
+++ static/ng-services/modelService.js
@@ -7,6 +7,8 @@ var service = angular.module('modelService', []);
 service.factory('ModelService', function($rootScope, $location) {
     var model = {};
     model.current_project = null;
+    model.current_sample = null;
+    model.current_panel_template = null;
 
     model.setCurrentProject = function (value) {
         this.current_project = value;
@@ -15,6 +17,24 @@ service.factory('ModelService', function($rootScope, $location) {
 
     model.getCurrentProject = function () {
         return this.current_project;
+    };
+
+    model.setCurrentSample = function (value) {
+        this.current_sample = value;
+        $rootScope.$broadcast('sampleChanged');
+    };
+
+    model.getCurrentSample = function () {
+        return this.current_sample;
+    };
+
+    model.setCurrentPanelTemplate = function (value) {
+        this.current_panel_template = value;
+        $rootScope.$broadcast('panelTemplateChanged');
+    };
+
+    model.getCurrentPanelTemplate = function () {
+        return this.current_panel_template;
     };
 
     return model;
