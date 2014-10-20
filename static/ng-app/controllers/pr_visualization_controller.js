bedd5cbb11ea328842377c39578c6b4905518200
Scott White <whitews@gmail.com>
2014-10-20 12:32:33 -0400

    implement sample choosing and SampleCluster retrieval for PR visualization

--- static/ng-app/controllers/pr_visualization_controller.js
+++ static/ng-app/controllers/pr_visualization_controller.js
@@ -8,10 +8,35 @@ app.controller(
         function ($scope, $controller, $stateParams, ModelService) {
             // Inherits ProcessRequestController $scope
             $controller('ProcessRequestController', {$scope: $scope});
+            $scope.chosen_sample_id = null;
 
             $scope.process_request = ModelService.getProcessRequest(
                 $stateParams.requestId
             );
+
+            $scope.process_request.$promise.then(function () {
+                $scope.sample_collection = ModelService.getSampleCollectionMembers(
+                    $scope.process_request.sample_collection
+                )
+            });
+
+            $scope.$watch('chosen_sample_id', function() {
+                if ($scope.chosen_sample_id) {
+                    $scope.retrieving_data = true;
+                    initialize_plot();
+                }
+            });
+
+            function initialize_plot() {
+                $scope.sample_clusters = ModelService.getSampleClusters(
+                    $scope.process_request.id,
+                    $scope.chosen_sample_id
+                )
+
+                $scope.sample_clusters.$promise.then(function() {
+                    $scope.retrieving_data = false;
+                });
+            }
         }
     ]
 );
