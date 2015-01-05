99fa2720fc2710ae80fee857f1c2c85ae1e0b908
Scott White <whitews@gmail.com>
2015-01-05 15:53:40 -0500

    don't select TIME or NULL parameters by default in PR creation wizard (closes issue #9)

--- static/ng-app/controllers/process_request_controller.js
+++ static/ng-app/controllers/process_request_controller.js
@@ -316,12 +316,19 @@ app.controller(
                         }
                     });
                     $scope.model.parameters = [];
+                    var selected_param = true;
                     for (var i = 0; i < master_parameter_list.length; i++) {
                         if (indices_to_exclude.indexOf(i) == -1) {
+                            // by default, don't select NULL or TIME params
+                            if (["NUL", "TIM"].indexOf(master_parameter_list[i].substr(0, 3)) != -1) {
+                                selected_param = false;
+                            } else {
+                                selected_param = true;
+                            }
                             $scope.model.parameters.push(
                                 {
                                     parameter: master_parameter_list[i],
-                                    selected: true
+                                    selected: selected_param
                                 }
                             );
                         }
