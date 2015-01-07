da2bda42a350a80ca722a346e29e9aedd8bd875d
Scott White <whitews@gmail.com>
2015-01-07 10:38:58 -0500

    Disable "next" re-direction on reflow_app (see issue #6)

--- repository/views.py
+++ repository/views.py
@@ -9,7 +9,10 @@ def permission_denied(request):
     raise PermissionDenied
 
 
-@login_required
+# The anchors used by Angular, i.e. any paths starting with '#'
+# never make it to the server, so server side re-direction will
+# never really be useful, thus we disable 'redirect_field_name'
+@login_required(redirect_field_name=None)
 def reflow_app(request):
     return render_to_response(
         'reflow_app.html',
