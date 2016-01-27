5d5bf1933995b1b9db25f95eeb177a6b47c37dc1
Scott White <whitews@gmail.com>
2016-01-27 10:44:34 -0500

    Added controller for applying a cell subset label to multiple clusters

---
+++ static/ng-app/controllers/pr_multi_label_controller.js
@@ -0,0 +1,52 @@
+app.controller(
+    'PRMultiLabelController',
+    [
+        '$scope',
+        '$controller',
+        'ModelService',
+        function ($scope) {
+            // PRVisualizationController pushes necessary vars through to this
+            // controller
+
+            // some input defaults
+            $scope.instance.cell_subset = undefined;
+
+            $scope.instance.invalid = true;
+
+            $scope.check_inputs = function () {
+                var invalid = false;
+
+                if ($scope.instance.cell_subset === undefined) {
+                    invalid = true;
+                }
+
+                $scope.instance.invalid = invalid;
+            };
+
+            $scope.apply_labels = function () {
+                $scope.instance.clusters.forEach(function(c) {
+                    if (c.display_events === true) {
+                        // check if cluster already has this label
+                        var skip_cluster = false;
+                        for (var i = 0; i < c.labels.length; i++) {
+                            if (c.labels[i].label === $scope.instance.cell_subset.id) {
+                                skip_cluster = true;
+                                break;
+                            }
+                        }
+
+                        if (!skip_cluster) {
+                            $scope.instance.add_cluster_label(
+                                $scope.instance.cell_subset,
+                                c
+                            );
+                            c.labels.push($scope.instance.cell_subset)
+                        }
+                    }
+                });
+                // close modal
+                $scope.ok();
+            };
+        }
+    ]
+);
