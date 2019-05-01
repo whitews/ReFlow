5b11cf6260494be649ab693d9cd39c6688920fa1
Scott White <whitews@gmail.com>
2019-05-01 12:01:25 -0400

    Update urls.py

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from authenticate.views import login_view, logout_view, login_failed
 
 __author__ = 'swhite'
 
 urlpatterns = [
-    url(r'^login/?$', 'authenticate.views.login_view', name="login"),
-    url(r'^logout/?$', 'authenticate.views.logout_view', name="logout"),
-    url(r'^login_failed/?$', 'authenticate.views.login_failed', name="login_failed")
+    url(r'^login/?$', login_view, name="login"),
+    url(r'^logout/?$', logout_view, name="logout"),
+    url(r'^login_failed/?$', login_failed, name="login_failed")
 ]
