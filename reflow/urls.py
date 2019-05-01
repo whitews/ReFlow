ad51aa519c5228cffeefbb62cf3500be668cc938
Scott White <whitews@gmail.com>
2019-05-01 12:25:18 -0400

    Update urls.py

--- reflow/urls.py
+++ reflow/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
+from api_views import api_root
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_docs.views import documentation
 
 urlpatterns = [
     url(r'^', include('authenticate.urls')),
     url(r'^', include('repository.urls')),
     # (r'^admin/', include(admin.site.urls)),
-    url(r'^api/$', 'reflow.api_views.api_root'),
-    url(r'^api/token-auth/$', 'rest_framework.authtoken.views.obtain_auth_token', name='api-token-auth'),
+    url(r'^api/$', api_root),
+    url(r'^api/token-auth/$', obtain_auth_token, name='api-token-auth'),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/docs/', include('rest_framework_docs.urls', namespace='rest_framework_docs')),
-    url(r'^api/docs/$', 'rest_framework_docs.views.documentation', name='api-docs')
+    url(r'^api/docs/$', documentation, name='api-docs')
 ]
