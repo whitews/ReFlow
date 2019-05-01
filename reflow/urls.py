9be545ae805fa088d6740df786627c7fbb2a2db6
Scott White <whitews@gmail.com>
2019-05-01 12:32:23 -0400

    Update urls.py

--- reflow/urls.py
+++ reflow/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 admin.autodiscover()
 from api_views import api_root
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_docs.views import documentation
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='ReFlow API')
 
 urlpatterns = [
     url(r'^', include('authenticate.urls')),
@@ -12,6 +14,5 @@ urlpatterns = [
     url(r'^api/$', api_root),
     url(r'^api/token-auth/$', obtain_auth_token, name='api-token-auth'),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/docs/', include('rest_framework_docs.urls', namespace='rest_framework_docs')),
-    url(r'^api/docs/$', documentation, name='api-docs')
+    url(r'^api/docs/', schema_view)
 ]
